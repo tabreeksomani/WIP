db5db02d03529e2f687d60d801a65d2ec9f18895
tsomani <tabreek.somani@gmail.com>
2023-07-18 10:00:11 -0700

    Programs Style Updated

--- src/components/Programs.vue
+++ src/components/Programs.vue
@@ -1,96 +1,133 @@
 <template>
-    <div class="card-container">
-        <h2>Our Programs</h2>
-        <v-card width="344">
-            <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover></v-img>
+    <div style="background-color:blanchedalmond">
+        <h2>Upcoming Events</h2>
 
-            <v-card-title> Podcast </v-card-title>
+        <div class="card-container">
+            <h2>Our Programs</h2>
 
-            <v-card-subtitle> Inspiring Confidence, Shaping the Future </v-card-subtitle>
+            <v-card width="344" v-hover="{ classSelector: '.hover-effect' }">
+                <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover></v-img>
 
-            <v-card-actions>
-                <v-btn color="orange-lighten-2" variant="text" class="customLearn"> Explore </v-btn>
+                <v-card-title> Podcast </v-card-title>
 
-                <v-spacer></v-spacer>
+                <v-card-subtitle> Inspiring Confidence, Shaping the Future </v-card-subtitle>
 
-                <v-btn :icon="show1 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show1 = !show1"
-                    class="customArrow"></v-btn>
-            </v-card-actions>
+                <v-card-actions>
+                    <v-btn color="orange-lighten-2" variant="text" class="customLearn"> Explore </v-btn>
 
-            <v-expand-transition>
-                <div v-show="show1">
-                    <v-divider></v-divider>
+                    <v-spacer></v-spacer>
 
-                    <v-card-text>
-                        I'm a thing. But, like most politicians, he promised more than he
-                        could deliver. You won't have time for sleeping, soldier, not with all
-                        the bed making you'll be doing. Then we'll go with that data file!
-                        Hey, you add a one and two zeros to that or we walk! You're going to
-                        do his laundry? I've got to find a way to escape.
-                    </v-card-text>
-                </div>
-            </v-expand-transition>
-        </v-card>
-        <v-card width="344">
-            <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover></v-img>
+                    <v-btn :icon="show1 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show1 = !show1"
+                        class="customArrow"></v-btn>
+                </v-card-actions>
 
-            <v-card-title> Dear Ally </v-card-title>
+                <v-expand-transition>
+                    <div v-show="show1">
+                        <v-divider></v-divider>
 
-            <v-card-subtitle> Advice from your big sister, one question away </v-card-subtitle>
+                        <v-card-text>
+                            I'm a thing. But, like most politicians, he promised more than he
+                            could deliver. You won't have time for sleeping, soldier, not with all
+                            the bed making you'll be doing. Then we'll go with that data file!
+                            Hey, you add a one and two zeros to that or we walk! You're going to
+                            do his laundry? I've got to find a way to escape.
+                        </v-card-text>
+                    </div>
+                </v-expand-transition>
+            </v-card>
+            <v-card width="344">
+                <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover></v-img>
 
-            <v-card-actions>
-                <v-btn color="orange-lighten-2" variant="text" class="customLearn"> Explore </v-btn>
+                <v-card-title> Dear Ally </v-card-title>
 
-                <v-spacer></v-spacer>
+                <v-card-subtitle> Advice from your big sister, one question away </v-card-subtitle>
 
-                <v-btn :icon="show2 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show2 = !show2"
-                    class="customArrow"></v-btn>
-            </v-card-actions>
+                <v-card-actions>
+                    <v-btn color="orange-lighten-2" variant="text" class="customLearn"> Explore </v-btn>
 
-            <v-expand-transition>
-                <div v-show="show2">
-                    <v-divider></v-divider>
+                    <v-spacer></v-spacer>
 
-                    <v-card-text>
-                        I'm a thing. But, like most politicians, he promised more than he
-                        could deliver. You won't have time for sleeping, soldier, not with all
-                        the bed making you'll be doing. Then we'll go with that data file!
-                        Hey, you add a one and two zeros to that or we walk! You're going to
-                        do his laundry? I've got to find a way to escape.
-                    </v-card-text>
-                </div>
-            </v-expand-transition>
-        </v-card>
-        <v-card width="344">
-            <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover></v-img>
+                    <v-btn :icon="show2 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show2 = !show2"
+                        class="customArrow"></v-btn>
+                </v-card-actions>
 
-            <v-card-title> Workshops </v-card-title>
+                <v-expand-transition>
+                    <div v-show="show2">
+                        <v-divider></v-divider>
 
-            <v-card-subtitle> Life School 101 </v-card-subtitle>
+                        <v-card-text>
+                            I'm a thing. But, like most politicians, he promised more than he
+                            could deliver. You won't have time for sleeping, soldier, not with all
+                            the bed making you'll be doing. Then we'll go with that data file!
+                            Hey, you add a one and two zeros to that or we walk! You're going to
+                            do his laundry? I've got to find a way to escape.
+                        </v-card-text>
+                    </div>
+                </v-expand-transition>
+            </v-card>
+            <v-card width="344">
+                <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover></v-img>
 
-            <v-card-actions>
-                <v-btn color="orange-lighten-2" variant="text" class="customLearn"> Explore </v-btn>
+                <v-card-title> Workshops </v-card-title>
 
-                <v-spacer></v-spacer>
+                <v-card-subtitle> Life School 101 </v-card-subtitle>
 
-                <v-btn :icon="show3 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show3 = !show3"
-                    class="customArrow"></v-btn>
-            </v-card-actions>
+                <v-card-actions>
+                    <v-btn color="orange-lighten-2" variant="text" class="customLearn"> Explore </v-btn>
 
-            <v-expand-transition>
-                <div v-show="show3">
-                    <v-divider></v-divider>
+                    <v-spacer></v-spacer>
 
-                    <v-card-text>
-                        I'm a thing. But, like most politicians, he promised more than he
-                        could deliver. You won't have time for sleeping, soldier, not with all
-                        the bed making you'll be doing. Then we'll go with that data file!
-                        Hey, you add a one and two zeros to that or we walk! You're going to
-                        do his laundry? I've got to find a way to escape.
-                    </v-card-text>
-                </div>
-            </v-expand-transition>
-        </v-card>
+                    <v-btn :icon="show3 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show3 = !show3"
+                        class="customArrow"></v-btn>
+                </v-card-actions>
+
+                <v-expand-transition>
+                    <div v-show="show3">
+                        <v-divider></v-divider>
+
+                        <v-card-text>
+                            I'm a thing. But, like most politicians, he promised more than he
+                            could deliver. You won't have time for sleeping, soldier, not with all
+                            the bed making you'll be doing. Then we'll go with that data file!
+                            Hey, you add a one and two zeros to that or we walk! You're going to
+                            do his laundry? I've got to find a way to escape.
+                        </v-card-text>
+                    </div>
+                </v-expand-transition>
+            </v-card>
+
+            <v-card width="344">
+                <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover></v-img>
+
+                <v-card-title> Blog </v-card-title>
+
+                <v-card-subtitle> Keep Up with Global Accounts of Progress for Women </v-card-subtitle>
+
+                <v-card-actions>
+                    <v-btn color="orange-lighten-2" variant="text" class="customLearn"> Explore </v-btn>
+
+                    <v-spacer></v-spacer>
+
+                    <v-btn :icon="show4 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show4 = !show4"
+                        class="customArrow"></v-btn>
+                </v-card-actions>
+
+                <v-expand-transition>
+                    <div v-show="show4">
+                        <v-divider></v-divider>
+
+                        <v-card-text>
+                            I'm a thing. But, like most politicians, he promised more than he
+                            could deliver. You won't have time for sleeping, soldier, not with all
+                            the bed making you'll be doing. Then we'll go with that data file!
+                            Hey, you add a one and two zeros to that or we walk! You're going to
+                            do his laundry? I've got to find a way to escape.
+                        </v-card-text>
+                    </div>
+                </v-expand-transition>
+            </v-card>
+        </div>
+
     </div>
 </template>
   
@@ -101,11 +138,20 @@ export default {
         show1: false,
         show2: false,
         show3: false,
+        show4: false,
     }),
 }
 </script>
   
 <style scoped>
+body {
+    background-color: aqua;
+}
+
+.card {
+    transition: box-shadow 0.3s;
+}
+
 .customeLearn {
     background-color: white;
 }
@@ -114,9 +160,13 @@ export default {
     color: black;
 }
 
+.hover-effect {
+    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
+}
+
 .card-container {
     display: flex;
-    justify-content: center;
+    justify-content: right;
     padding: 15px;
     margin-left: 30px;
     margin-right: 30px;
@@ -125,5 +175,6 @@ export default {
 
 .v-card {
     margin-left: 40px;
+    margin-bottom: 10px;
 }
 </style>
