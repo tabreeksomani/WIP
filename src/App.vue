4be50000ff7ab257b1e7e4fa11e4dd0da37499f0
tsomani <tabreek.somani@gmail.com>
2023-07-18 10:01:28 -0700

    Added elements to home page

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,27 @@
   <app-navigation></app-navigation>
 
   <feature-carousel></feature-carousel>
+  <WhoWeAre></WhoWeAre>
 
   <our-programs></our-programs>
+  <PodcastEpisodes></PodcastEpisodes>
 </template>
 
 <script>
 import AppNavigation from '@/components/AppNavigation';
 import FeatureCarousel from '@/components/Carousel.vue';
 import OurPrograms from '@/components/Programs.vue';
+import PodcastEpisodes from '@/components/Podcast.vue';
+import WhoWeAre from './components/WhoWeAre.vue';
 
 export default {
   name: 'App',
   components: {
     AppNavigation,
     FeatureCarousel,
-    OurPrograms
+    OurPrograms,
+    PodcastEpisodes,
+    WhoWeAre
   }
 };
 </script>
