027271859496133eb14b3202814c58e3356ebc90
tsomani <tabreek.somani@gmail.com>
2023-07-18 10:01:11 -0700

    Who We Are Section

---
+++ src/components/WhoWeAre.vue
@@ -0,0 +1,13 @@
+<template>
+    <div style="height: 100vh;">
+        <h2>Our Partners</h2>
+        <h2>Who We Are </h2>
+        <h2>Our Impact</h2>
+    </div>
+</template>
+<script>
+export default {
+    name: 'WhoWeAre'
+}
+</script>
+<style scoped></style>
