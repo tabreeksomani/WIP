ac8f9e67368cfced34c2944f6a6803e91bf40877
tsomani <tabreek.somani@gmail.com>
2023-07-20 12:15:53 -0700

    Added New Sections to Home Page

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,16 @@
   <app-navigation></app-navigation>
 
   <feature-carousel></feature-carousel>
+  <our-partners></our-partners>
   <WhoWeAre></WhoWeAre>
+
 
   <our-programs></our-programs>
   <PodcastEpisodes></PodcastEpisodes>
+  <featured-content></featured-content>
+  <get-involved></get-involved>
+  <membership-section></membership-section>
+  <page-footer></page-footer>
 </template>
 
 <script>
@@ -14,6 +20,11 @@ import FeatureCarousel from '@/components/Carousel.vue';
 import OurPrograms from '@/components/Programs.vue';
 import PodcastEpisodes from '@/components/Podcast.vue';
 import WhoWeAre from './components/WhoWeAre.vue';
+import PageFooter from './components/Footer.vue';
+import OurPartners from './components/OurPartners.vue';
+import FeaturedContent from './components/FeaturedContent.vue';
+import GetInvolved from './components/GetInvolved.vue';
+import MembershipSection from './components/Membership.vue';
 
 export default {
   name: 'App',
@@ -22,7 +33,12 @@ export default {
     FeatureCarousel,
     OurPrograms,
     PodcastEpisodes,
-    WhoWeAre
+    WhoWeAre,
+    FeaturedContent,
+    PageFooter,
+    OurPartners,
+    GetInvolved,
+    MembershipSection
   }
 };
 </script>
