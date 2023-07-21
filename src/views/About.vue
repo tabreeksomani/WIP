a67ac96c8ee2d622f4fd8fe338ac0064e94ed67d
tsomani <tabreek.somani@gmail.com>
2023-07-21 11:34:34 -0700

    About Page Structure

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+    <div style="height:100vh" id="story">
+        <h1>Our Story</h1>
+    </div>
+    <div style="height:100vh" id="team">
+        <h1>Our Team</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AboutPage',
+};
+</script>
