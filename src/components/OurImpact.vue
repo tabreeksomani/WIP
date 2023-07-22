83db5b57c09a4471e658746291f372bcc1d52d70
tsomani <tabreek.somani@gmail.com>
2023-07-22 10:50:33 -0700

    Refactored Impact and Partners

---
+++ src/components/OurImpact.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="impact-section">
+        <h2 class="section-title">Our Impact</h2>
+        <div class="stats">
+            <div class="stat">
+                <div class="stat-value">1000+</div>
+                <div class="stat-label">Projects Completed</div>
+            </div>
+            <div class="stat">
+                <div class="stat-value">10,000+</div>
+                <div class="stat-label">Lives Impacted</div>
+            </div>
+            <div class="stat">
+                <div class="stat-value">500+</div>
+                <div class="stat-label">Volunteers Engaged</div>
+            </div>
+            <!-- Add more stats if needed -->
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'OurImpact',
+};
+</script>
+  
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
+
+.impact-section {
+    background-color: #f7f7f7;
+    padding: 40px 0;
+    text-align: center;
+}
+
+.section-title {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: bold;
+    font-size: 24px;
+    margin-bottom: 20px;
+}
+
+.stats {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+}
+
+.stat {
+    width: 200px;
+    margin: 20px;
+}
+
+.stat-value {
+    font-size: 40px;
+    font-weight: bold;
+    color: #333;
+}
+
+.stat-label {
+    font-size: 16px;
+    color: #666;
+}
+</style>
+  
--- src/components/OurPartners.vue
+++ src/components/OurPartners.vue
@@ -1,72 +1,30 @@
 <template>
-    <div>
-        <div class="impact-section">
-            <h2 class="section-title">Our Impact</h2>
-            <div class="stats">
-                <div class="stat">
-                    <div class="stat-value">1000+</div>
-                    <div class="stat-label">Projects Completed</div>
-                </div>
-                <div class="stat">
-                    <div class="stat-value">10,000+</div>
-                    <div class="stat-label">Lives Impacted</div>
-                </div>
-                <div class="stat">
-                    <div class="stat-value">500+</div>
-                    <div class="stat-label">Volunteers Engaged</div>
-                </div>
-                <!-- Add more stats if needed -->
-            </div>
-        </div>
-
-        <div class="partners-section">
-            <h2 class="section-title">Our Partners</h2>
-            <div class="partner-logos">
-                <img v-for="(partner, index) in partners" :key="index" :src="partner.logo" alt="Partner Logo"
-                    class="partner-logo" />
-            </div>
+    <div class="partners-section">
+        <h2 class="section-title">Our Partners</h2>
+        <div class="partner-logos">
+            <img v-for="(partner, index) in partners" :key="index" :src="partner.logo" alt="Partner Logo"
+                class="partner-logo" />
         </div>
     </div>
 </template>
   
+<script>
+export default {
+    name: 'OurPartners',
+    data() {
+        return {
+            partners: [
+                { logo: 'partner1.png' },
+                { logo: 'partner2.png' },
+                { logo: 'partner3.png' },
+                // Add more partner objects with their logo paths
+            ],
+        };
+    },
+};
+</script>
+  
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap');
-
-.impact-section {
-    background-color: #f7f7f7;
-    padding: 40px 0;
-    text-align: center;
-}
-
-.section-title {
-    font-family: 'Montserrat', sans-serif;
-    font-weight: bold;
-    font-size: 24px;
-    margin-bottom: 20px;
-}
-
-.stats {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-}
-
-.stat {
-    width: 200px;
-    margin: 20px;
-}
-
-.stat-value {
-    font-size: 40px;
-    font-weight: bold;
-    color: #333;
-}
-
-.stat-label {
-    font-size: 16px;
-    color: #666;
-}
-
 .partners-section {
     height: 20vh;
     display: flex;
@@ -89,18 +47,3 @@
 }
 </style>
   
-<script>
-export default {
-    name: 'OurPartners',
-    data() {
-        return {
-            partners: [
-                { logo: 'partner1.png' },
-                { logo: 'partner2.png' },
-                { logo: 'partner3.png' },
-                // Add more partner objects with their logo paths
-            ],
-        };
-    },
-};
-</script>
