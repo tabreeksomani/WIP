6b8fcf0fb555544e10709db24917e5fa7ef0fc62
tsomani <tabreek.somani@gmail.com>
2023-07-24 09:46:19 -0700

    Created Our Team Component Structure

---
+++ src/components/OurTeam.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="our-team">
+        <v-container>
+            <v-row justify="center">
+                <!-- Co-Presidents -->
+                <v-col cols="12">
+                    <h2 class="text-center">Co-Presidents</h2>
+                </v-col>
+                <v-col v-for="coPresident in coPresidents" :key="coPresident.name" cols="12" sm="6" md="4">
+                    <v-card class="team-member" elevation="10">
+                        <v-avatar size="150" class="avatar">
+                            <v-img :src="coPresident.image" :alt="coPresident.name"></v-img>
+                        </v-avatar>
+                        <v-card-title class="text-h5 text-center">{{ coPresident.name }}</v-card-title>
+                        <v-card-subtitle class="text-center">{{ coPresident.position }}</v-card-subtitle>
+                    </v-card>
+                </v-col>
+
+                <!-- Board of Directors -->
+                <v-col cols="12">
+                    <h2 class="text-center">Board of Directors</h2>
+                </v-col>
+                <v-col v-for="index in 7" :key="index" cols="12" sm="6" md="4">
+                    <v-card class="team-member" elevation="10">
+                        <v-avatar size="150" class="avatar">
+                            <!-- Use placeholder image URLs here -->
+                            <v-img :src="getBoardMemberImage(index)" :alt="'Board Member ' + index"></v-img>
+                        </v-avatar>
+                        <v-card-title class="text-h5 text-center">Board Member {{ index }}</v-card-title>
+                        <v-card-subtitle class="text-center">Board of Director</v-card-subtitle>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </div>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            coPresidents: [
+                {
+                    name: "Co-President 1",
+                    position: "Co-President",
+                    image: "https://via.placeholder.com/150", // Replace with actual co-president image URL
+                },
+                {
+                    name: "Co-President 2",
+                    position: "Co-President",
+                    image: "https://via.placeholder.com/150", // Replace with actual co-president image URL
+                },
+            ],
+        };
+    },
+    methods: {
+        getBoardMemberImage(index) {
+            // Use placeholder image URLs for board members
+            return `https://via.placeholder.com/150?text=Board+Member+${index}`;
+        },
+    },
+};
+</script>
+  
+<style>
+/* Customize the styles as per your preference */
+.our-team {
+    padding: 20px;
+}
+
+.team-member {
+    margin: 10px;
+    text-align: center;
+    padding-top: 10px;
+}
+
+.avatar {
+    margin: 0 auto;
+}
+</style>
+  
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,13 +2,18 @@
     <div style="height:100vh" id="story">
         <h1>Our Story</h1>
     </div>
-    <div style="height:100vh" id="team">
+    <div style="min-height:100vh" id="team">
         <h1>Our Team</h1>
+        <OurTeam></OurTeam>
     </div>
 </template>
 
 <script>
+import OurTeam from '@/components/OurTeam.vue';
 export default {
     name: 'AboutPage',
+    components: {
+        OurTeam
+    }
 };
 </script>
