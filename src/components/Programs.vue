731380f9326d9e78af1dde05df20be5e43df3b13
tsomani <tabreek.somani@gmail.com>
2023-07-29 10:25:08 -0700

    Programs Structure

--- src/components/Programs.vue
+++ src/components/Programs.vue
@@ -1,140 +1,32 @@
 <template>
-    <div style="background-color:blanchedalmond" class="main-container">
-        <!-- <h2>Upcoming Events</h2>
-        <h2>Programs</h2> -->
-        <div class="card-container">
-
-            <v-card width="344" v-hover="{ classSelector: '.hover-effect' }">
-                <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover></v-img>
-
-                <v-card-title> Podcast </v-card-title>
-
-                <v-card-subtitle> Inspiring Confidence, Shaping the Future </v-card-subtitle>
-
-                <v-card-actions>
-                    <v-btn color="orange-lighten-2" variant="text" class="customLearn"> Explore </v-btn>
-
-                    <v-spacer></v-spacer>
-
-                    <v-btn :icon="show1 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show1 = !show1"
-                        class="customArrow"></v-btn>
-                </v-card-actions>
-
-                <v-expand-transition>
-                    <div v-show="show1">
-                        <v-divider></v-divider>
-
-                        <v-card-text>
-                            I'm a thing. But, like most politicians, he promised more than he
-                            could deliver. You won't have time for sleeping, soldier, not with all
-                            the bed making you'll be doing. Then we'll go with that data file!
-                            Hey, you add a one and two zeros to that or we walk! You're going to
-                            do his laundry? I've got to find a way to escape.
-                        </v-card-text>
+    <div style="display: flex; background-color: rgb(254 253 234);">
+        <div class="program">
+            <h2>Our Programs</h2>
+            <div class="content-grid">
+                <div class="blog-post" v-for="post in blogPosts" :key="post.id">
+                    <div class="blog-post-card">
+                        <div class="blog-post-thumbnail">
+                            <!-- Replace with your blog post thumbnail -->
+                            <img :src="post.thumbnail" alt="Blog Post Thumbnail" />
+                        </div>
+                        <h4 class="blog-post-title">{{ post.title }}</h4>
+                        <p class="blog-post-description">
+                            {{ post.excerpt }}</p>
+                        <div class="blog-post-link">
+                            <a :href="post.url">Read More</a>
+                            <i class="fas fa-arrow-right"></i>
+                        </div>
                     </div>
-                </v-expand-transition>
-            </v-card>
-            <v-card width="344">
-                <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover></v-img>
-
-                <v-card-title> Dear Ally </v-card-title>
-
-                <v-card-subtitle> Advice from your big sister, one question away </v-card-subtitle>
-
-                <v-card-actions>
-                    <v-btn color="orange-lighten-2" variant="text" class="customLearn"> Explore </v-btn>
-
-                    <v-spacer></v-spacer>
-
-                    <v-btn :icon="show2 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show2 = !show2"
-                        class="customArrow"></v-btn>
-                </v-card-actions>
-
-                <v-expand-transition>
-                    <div v-show="show2">
-                        <v-divider></v-divider>
-
-                        <v-card-text>
-                            I'm a thing. But, like most politicians, he promised more than he
-                            could deliver. You won't have time for sleeping, soldier, not with all
-                            the bed making you'll be doing. Then we'll go with that data file!
-                            Hey, you add a one and two zeros to that or we walk! You're going to
-                            do his laundry? I've got to find a way to escape.
-                        </v-card-text>
-                    </div>
-                </v-expand-transition>
-            </v-card>
-            <v-card width="344">
-                <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover></v-img>
-
-                <v-card-title> Workshops </v-card-title>
-
-                <v-card-subtitle> Life School 101 </v-card-subtitle>
-
-                <v-card-actions>
-                    <v-btn color="orange-lighten-2" variant="text" class="customLearn"> Explore </v-btn>
-
-                    <v-spacer></v-spacer>
-
-                    <v-btn :icon="show3 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show3 = !show3"
-                        class="customArrow"></v-btn>
-                </v-card-actions>
-
-                <v-expand-transition>
-                    <div v-show="show3">
-                        <v-divider></v-divider>
-
-                        <v-card-text>
-                            I'm a thing. But, like most politicians, he promised more than he
-                            could deliver. You won't have time for sleeping, soldier, not with all
-                            the bed making you'll be doing. Then we'll go with that data file!
-                            Hey, you add a one and two zeros to that or we walk! You're going to
-                            do his laundry? I've got to find a way to escape.
-                        </v-card-text>
-                    </div>
-                </v-expand-transition>
-            </v-card>
-
-            <v-card width="344">
-                <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px" cover></v-img>
-
-                <v-card-title> Blog </v-card-title>
-
-                <v-card-subtitle> Keep Up with Global Accounts of Progress for Women </v-card-subtitle>
-
-                <v-card-actions>
-                    <v-btn color="orange-lighten-2" variant="text" class="customLearn"> Explore </v-btn>
-
-                    <v-spacer></v-spacer>
-
-                    <v-btn :icon="show4 ? 'mdi-chevron-up' : 'mdi-chevron-down'" @click="show4 = !show4"
-                        class="customArrow"></v-btn>
-                </v-card-actions>
-
-                <v-expand-transition>
-                    <div v-show="show4">
-                        <v-divider></v-divider>
-
-                        <v-card-text>
-                            I'm a thing. But, like most politicians, he promised more than he
-                            could deliver. You won't have time for sleeping, soldier, not with all
-                            the bed making you'll be doing. Then we'll go with that data file!
-                            Hey, you add a one and two zeros to that or we walk! You're going to
-                            do his laundry? I've got to find a way to escape.
-                        </v-card-text>
-                    </div>
-                </v-expand-transition>
-            </v-card>
+                </div>
+            </div>
         </div>
-
-        <div class="upcoming-events-container">
-            <upcoming-events></upcoming-events>
-        </div>
+        <upcoming-events></upcoming-events>
     </div>
 </template>
   
 <script>
 import UpcomingEvents from './UpcomingEvents.vue';
+
 export default {
     name: 'OurPrograms',
     components: {
@@ -142,20 +34,127 @@ export default {
     },
     data: () => ({
         show1: false,
-        show2: false,
-        show3: false,
-        show4: false,
-    }),
+        blogPosts: [
+            {
+                id: 1,
+                title: "Dear Ally",
+                excerpt: "How to make conversation with strangers in awkward situations",
+                url: "https://www.example.com/blog-post-1",
+                thumbnail: '/coffee.jpeg'
+            },
+            {
+                id: 2,
+                title: "Workshops",
+                excerpt:
+                    "How to gain confidence to speak at conferences even if you're just starting out",
+                url: "https://www.example.com/blog-post-2",
+                thumbnail: '/question.png'
+            },
+            {
+                id: 3,
+                title: "Podcast",
+                excerpt:
+                    "How to gain confidence to speak at conferences even if you're just starting out",
+                url: "https://www.example.com/blog-post-2",
+                thumbnail: '/question.png'
+            },
+            {
+                id: 4,
+                title: "Cohorts",
+                excerpt:
+                    "How to gain confidence to speak at conferences even if you're just starting out",
+                url: "https://www.example.com/blog-post-2",
+                thumbnail: '/question.png'
+            },
+            // Add more blog posts as needed
+        ],
+        // Add similar data properties for other cards
+    })
 }
 </script>
   
 <style scoped>
-body {
-    background-color: aqua;
+.cta-button {
+    display: inline-block;
+    margin-top: 10px;
+    padding: 8px 16px;
+    background-color: #a30b37;
+    color: white;
+    text-decoration: none;
+    border-radius: 4px;
+    transition: background-color 0.3s ease;
 }
 
-.card {
-    transition: box-shadow 0.3s;
+.cta-button:hover {
+    background-color: #7c0622;
+}
+
+.program {
+    width: 50%;
+    padding: 20px;
+    padding-right: 0px;
+}
+
+/* Styles for the blog post cards */
+.content-grid {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    /* 2x2 grid layout */
+    grid-gap: 20px;
+    margin-top: 60px;
+    margin-left: 20px;
+    padding-top: 40px;
+    max-width: 800px;
+    margin: 0 auto;
+}
+
+.blog-post-card {
+    background-color: white;
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    padding: 20px;
+    min-width: 400px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    text-align: center;
+}
+
+.blog-post-thumbnail img {
+    width: 80px;
+    height: 80px;
+    object-fit: cover;
+    border-radius: 50%;
+    margin-bottom: 10px;
+}
+
+.blog-post-title {
+    font-size: 18px;
+    font-weight: bold;
+    margin-bottom: 5px;
+}
+
+.blog-post-link {
+    margin-top: 15px;
+}
+
+.blog-post-link a {
+    color: #a30b37;
+    text-decoration: underline;
+}
+
+.blog-post-link i {
+    color: #a30b37;
+    margin-left: 5px;
+    font-size: 16px;
+    transition: transform 0.3s ease;
+}
+
+.blog-post-link a:hover,
+.blog-post-link i:hover {
+    transform: translateX(5px);
+}
+
+body {
+    background-color: aqua;
 }
 
 .main-container {
@@ -167,8 +166,7 @@ body {
 
 .card-container {
     display: grid;
-    grid-template-columns: 3fr 2fr;
-    /* Adjust the width distribution here (e.g., 3fr and 2fr) */
+    grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
     gap: 20px;
     padding: 10px;
 }
@@ -177,20 +175,39 @@ body {
     padding: 10px;
 }
 
-
-.customeLearn {
-    background-color: white;
+.custom-card {
+    transition: box-shadow 0.3s;
+    background-color: #FFF5E6;
+    /* Adjust the background color */
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    text-align: center;
 }
 
-.customArrow {
-    color: black;
+.custom-title {
+    font-size: 20px;
+    font-weight: bold;
+    color: #A30B37;
+    /* Adjust the title color */
+}
+
+.custom-subtitle {
+    color: #666;
+    /* Adjust the subtitle color */
+}
+
+.custom-learn {
+    color: #A30B37;
+    /* Adjust the 'Explore' button text color */
+}
+
+.custom-arrow {
+    color: #A30B37;
+    /* Adjust the chevron icon color */
 }
 
 .hover-effect {
     box-shadow: 0 2px 10px rgba(0, 0, 0, 0.3);
 }
-
-
 
 .v-card-text {
     padding: 0;
@@ -201,3 +218,4 @@ body {
     margin-bottom: 10px;
 }
 </style>
+  
